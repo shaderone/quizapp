fa7807de1fc85b35c9e44597abe44c48e5b18698
pixiedust07 <sharonshaji127001@gmail.com>
2024-02-22 22:43:18 +0530

    Read data from json(from assets) others : dependency injection

--- lib/controllers/question_paper/data_uploader.dart
+++ lib/controllers/question_paper/data_uploader.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 //This controller is for the admin
@@ -11,7 +14,21 @@ class DataUploader extends GetxController {
     super.onReady();
   }
 
-  void uploadData() {
+  Future<void> uploadData() async {
     print("Data Uploading...");
+    // to upload files :
+    // ** how 'context' is accessible here, - through getX, but how?
+    // the loadString() will load the 'assets' key from pubspec.yaml (which includes every directory mentioned under assets:)
+    final manifestContent = await DefaultAssetBundle.of(Get.context!).loadString("AssetManifest.json");
+    //now the whole data is loaded into 'manifestContent', and next we need to decode the json data to read its content, (it will return a MAP)
+    final Map<String, dynamic> manifestContentMap = json.decode(manifestContent);
+    //now to filter out the specific data that's requried (in this case we only need the json files stored in under assets/DB/paper, however the result has all data under 'assets:'), we need to get the keys
+    // ** what is the difference between 'Iterable' and 'List'
+    final manifestContentFilteredIterable = manifestContentMap.keys.where((path) => path.startsWith("assets/DB/paper") && path.contains(".json"));
+    // this data is what admin manages
+    final papersInAssets = manifestContentFilteredIterable.toList();
+    print(papersInAssets);
+
+    // ? NEXT : loop through the filtered data.
   }
 }
